c94184ff2e3630bc006f7c2a4a55ea072ba9e5cb
Will Pringle <will@distributive.network>
2024-09-29 13:46:14 -0400

    lint: specify nodejs environment in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
 ];
+
